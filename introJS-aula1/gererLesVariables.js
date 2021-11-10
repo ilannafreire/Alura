8c721b0b16642e42a54cadad78d168a2dc9b36ff
ilannafreire <ilannafreiret@hotmail.com>
2021-11-10 03:52:06 -0300

    traiter avec js

---
+++ introJS-aula1/gererLesVariables.js
@@ -0,0 +1,8 @@
+console.log("Gerer les variables");
+//js is case sensitive
+
+const idade = 29;
+const nome = "Salete";
+
+//dont do it 
+//month = "january";
---
+++ introJS-aula1/operationsArithmetiques.js
@@ -0,0 +1,3 @@
+console.log("operations Arithmetiques");
+console.log(10 + 8 * 2);
+console.log(2+2);
